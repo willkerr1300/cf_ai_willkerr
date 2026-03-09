97a7dfc5f9017afa1c11a54e920fc6c90439ca4e
willkerr-uva <yun5pc@virginia.edu>
2026-03-09 13:24:50 -0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+    alias: {
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    },
+  },
   build: {
     outDir: "dist/client",
     emptyOutDir: true,
